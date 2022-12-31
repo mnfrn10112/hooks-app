a70e7bbe7ae78b6e92124fd7a23a58d03ac60c08
Manuel Gil <manuel.gil@services.mss.pe>
2022-12-31 14:31:56 -0500

    useRef

---
+++ src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+
+export const FocusScreen = () => {
+
+    const inputRef = useRef();
+    console.log(inputRef);
+    const onFocus = () => { 
+        inputRef.current.select()
+     }
+    return (
+        <>
+            <h1>Focus Screen</h1>
+            <hr />
+
+            <input
+                ref={inputRef}
+                type="text"
+                placeholder="Ingrese su nombre"
+                className="form-control"
+            />
+
+            <button onClick={onFocus} className="btn btn-primary mt-2">
+                Set focus
+            </button>
+        </>
+    );
+};
