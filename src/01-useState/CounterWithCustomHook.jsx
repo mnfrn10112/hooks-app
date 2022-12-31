8752c582c080a61a33e46025b9c02b91b0ed716f
Manuel Gil <manuel.gil@services.mss.pe>
2022-12-31 00:05:05 -0500

    useState with hook

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,14 @@
+import { useCounter } from "../hooks/useCounter";
+
+export const CounterWithCustomHook = () => {
+    const { counter, increment, decrement, reset } = useCounter();
+    return (
+        <>
+            <h1>Couter with hook: {counter}</h1>
+            <hr />
+            <button onClick={() => increment(2)} className="btn btn-primary">+1</button>
+            <button onClick={reset} className="btn btn-primary">reset</button>
+            <button onClick={() => decrement(2)} className="btn btn-primary">-1</button>
+        </>
+    );
+};
