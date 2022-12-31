1e38c5f9f82aa674c16b47fce4366a0133150631
Manuel Gil <manuel.gil@services.mss.pe>
2022-12-31 00:41:20 -0500

    useEffect return

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,17 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
+
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
-    console.log('Message Mounted');
-  
+
+    const onMouseMove = ({ x, y }) => {
+      const coords = { x, y };
+      setCoords(coords)
+      // console.log(coords);
+    };
+
+    window.addEventListener('mousemove', onMouseMove);
+
     return () => {
-      console.log('Message Unmounted')
-    }
-  }, [])
-  
+      window.removeEventListener('mousemove', onMouseMove);
+    };
+  }, []);
+
   return (
     <>
       <h3>Usuario ya existe.</h3>
+      {JSON.stringify(coords)}
     </>
   );
 };
