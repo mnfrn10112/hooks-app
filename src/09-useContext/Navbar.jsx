9b780dd16850948cdc041edacd14821c49f5be4a
Manuel Gil <manuel.gil@services.mss.pe>
2023-01-01 20:46:02 -0500

    NavLink

--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,11 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
     return (
         <>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/login">Login</Link>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-2">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">UseContext</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                                to='/' >
+                                Home
+                            </NavLink>
+                            <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                                to='/about' >
+                                About
+                            </NavLink>
+                            <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                                to='/login' >
+                                Login
+                            </NavLink>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </>
     );
 };
